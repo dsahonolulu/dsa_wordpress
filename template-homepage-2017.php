81ee1f530252b081ad4e193e9a0a5587c91b0169
Brett Jones <brettjonesdev@gmail.com>
2018-07-23 08:59:26 -1000

    Remove stray chars

--- template-homepage-2017.php
+++ template-homepage-2017.php
@@ -168,8 +168,7 @@ Template Name: Hompage 2017
 		<div id="dsa-home-row-3" class="bg-DSAred">
 			<div class="row dsa-home-row-3-edit">
 				<div class="text-center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/ui-foregrounds/calendar.svg" width="141" height="141" /><br /><h2 class="dsa-section-title txt-DSAwhite">Upcoming Events</h2></div>
-                ?>
-				<?php // Retrieve the next 5 upcoming events
+                <?php // Retrieve the next 5 upcoming events
 					$events = tribe_get_events( array(
 					    'posts_per_page' => 2,
 					    'start_date' => date( 'Y-m-d H:i:s', strtotime("-6 hours"))
